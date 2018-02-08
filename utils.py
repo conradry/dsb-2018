151bd74d0c4fc4ba20aadef29b96a36c0ea16878
Ubuntu <ubuntu@ubuntu.h4rg2ftduhyupnllaas5vkeqaa.bx.internal.cloudapp.net>
2018-02-08 17:28:54 +0000

    second commit

--- utils.py
+++ utils.py
@@ -30,6 +30,7 @@ from keras.preprocessing.image import ImageDataGenerator
 import bcolz
 from shutil import copyfile
 from mean_average_precision import *
+from PIL import Image
 
 K.set_image_data_format('channels_last')
 
@@ -143,6 +144,20 @@ def make_data_array(path, data_ids, get_masks=False):
         return imgs, msks, img_sizes
     else:
         return imgs, img_sizes
+    
+def rle_encoding(x):
+    '''
+    x: numpy array of shape (height, width), 1 - mask, 0 - background
+    Returns run length as list
+    '''
+    dots = np.where(x.T.flatten()==1)[0] # .T sets Fortran order down-then-right
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
 
 
 get_ipython().magic(u'matplotlib inline')
